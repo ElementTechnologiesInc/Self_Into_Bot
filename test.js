654e3179310478768ce0b0bc3d710ca8f28b6d96
samadritadas <samadrita.das1993@gmail.com>
2017-12-01 12:21:49 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     var notes= JSON.parse(localStorage.getItem("resume"));
+     console.log("Ready");
     
    $('.drawer').drawer({
+       console.log("Works");
         class: {
           nav: 'drawer-nav',
           toggle: 'drawer-toggle',
